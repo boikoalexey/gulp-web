066ba3530783c7d01fe494ac3d787e1d769fc9a9
boikoalexey <91904042+boikoalexey@users.noreply.github.com>
2021-10-13 20:06:27 +0300

    Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -16,3 +16,25 @@ const del = require("del");
 const panini = require("panini");
 const browsersync = require("browser-sync").create();
 
+
+var path = {
+    build: {
+        html: "dist/",
+        js: "dist/assets/js/",
+        css: "dist/assets/css",
+        images: "dist/assets/img"
+    },
+    src: {
+        html: "src/*.html",
+        js: "src/assets/js/*.js",
+        css: "src/assets/sass/style.css",
+        images: "src/assets/img/**/*.{jpg,png,svg,gif,ico}"
+    },
+    watch: {
+        html: "src/**/*.html",
+        js: "src/assets/js/**/*.js",
+        css: "src/assets/sass/**/*.scss",
+        images: "src/assets/img/**/*.{jpg,png,svg,gif,ico}"
+    },
+    clean: "./dist"
+}
